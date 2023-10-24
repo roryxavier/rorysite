0e40aa751884e978f66393d0ec92f8cca02dd2a4
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2023-10-24 23:22:41 +0800

    Update +layout.svelte
    - added meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,29 @@
 <script>
 	import '../app.css';
 	import RootLayout from '@/component/RootLayout.svelte';
+	import OgImage from '@/resource/avatar.webp';
 </script>
+
+<svelte:head>
+	<!-- Twitter Meta -->
+	<meta name="twitter:title" content="Rory Xavier" />
+	<meta name="twitter:image" content={OgImage} />
+	<meta
+		name="twitter:description"
+		content="Hey there! My name's Rory and I'm a Blue Wolf currently hanging out in Singapore."
+	/>
+
+	<!-- Meta -->
+	<meta name="title" property="og:title" content="Rory Xavier" />
+	<meta name="type" property="og:type" content="website" />
+	<meta name="image" property="og:image" content={OgImage} />
+	<meta name="url" property="og:url" content="https://rorywolf.net" />
+	<meta
+		name="description"
+		property="og:description"
+		content="Hey there! My name's Rory and I'm a Blue Wolf currently hanging out in Singapore."
+	/>
+</svelte:head>
 
 <body>
 	<RootLayout>
