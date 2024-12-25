9dc1027271da85640bffed4c36841a2962981562
Rory Xavier <84554571+roryxavier@users.noreply.github.com>
2024-12-25 23:29:18 +0800

    feat: add static image to the link
    - add static svg image to each link
    - add telegram sticker link url

--- src/models/Fursona.model.ts
+++ src/models/Fursona.model.ts
@@ -1,48 +1,54 @@
 import type { SocialLinkModel } from './SocialLink.model';
 
 export type FursonaModel = Readonly<{
-	labels: string[];
-	socialLinks: SocialLinkModel[];
+  labels: string[];
+  socialLinks: SocialLinkModel[];
 }>;
 
 export const RORY_CHRIS_FURSONA: FursonaModel = {
-	labels: ['He/Him', 'Wolf', 'Gay', 'Taken - 🦊', 'Programmer', 'Happy Go Lucky'],
-	socialLinks: [
-		{
-			name: '@roryxavier',
-			alt: 'Telegram',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg',
-			link: 'https://t.me/roryxavier',
-		},
-		{
-			name: 'Reference Website',
-			alt: 'Reference Site',
-			img: 'https://img.icons8.com/color/48/null/paint-palette-with-brush.png',
-			link: 'https://ref.rorywolf.net',
-		},
-		{
-			name: '@roryxavier',
-			alt: 'Bluesky',
-			img: 'https://bsky.app/static/favicon-32x32.png',
-			link: 'https://bsky.app/profile/roryxavier.bsky.social',
-		},
-		{
-			name: '@rory_xavi',
-			alt: 'Twitter',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/150px-Logo_of_Twitter.svg.png',
-			link: 'https://twitter.com/rory_xavi',
-		},
-		{
-			name: '@roryxavier',
-			alt: 'Github',
-			img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png',
-			link: 'https://github.com/roryxavier',
-		},
-		{
-			name: '@roryxavier',
-			alt: 'Discord',
-			img: 'https://www.svgrepo.com/show/353655/discord-icon.svg',
-			link: 'https://discordapp.com/users/525678985049538610',
-		},
-	],
+  labels: ['He/Him', 'Wolf', 'Gay', 'Taken - 🦊', 'Programmer', 'Happy Go Lucky'],
+  socialLinks: [
+    {
+      name: '@roryxavier',
+      alt: 'Telegram',
+      img: 'telegram',
+      link: 'https://t.me/roryxavier',
+    },
+    {
+      name: 'Reference Website',
+      alt: 'Reference Site',
+      img: 'gallery-white',
+      link: 'https://ref.rorywolf.net',
+    },
+    {
+      name: '@roryxavier',
+      alt: 'Bluesky',
+      img: 'bluesky',
+      link: 'https://bsky.app/profile/roryxavier.bsky.social',
+    },
+    {
+      name: '@rory_xavi',
+      alt: 'Twitter',
+      img: 'twitter',
+      link: 'https://twitter.com/rory_xavi',
+    },
+    {
+      name: '@roryxavier',
+      alt: 'Github',
+      img: 'github',
+      link: 'https://github.com/roryxavier',
+    },
+    {
+      name: '@roryxavier',
+      alt: 'Discord',
+      img: 'discord',
+      link: 'https://discordapp.com/users/525678985049538610',
+    },
+    {
+      name: 'Sticker Link',
+      alt: 'sticker link to telegram',
+      img: 'link',
+      link: 'https://t.me/addstickers/RoryChris',
+    },
+  ],
 };
--- src/routes/SocialLink.svelte
+++ src/routes/SocialLink.svelte
@@ -1,45 +1,50 @@
 <script lang="ts">
-	import type { SocialLinkModel } from '@/models/SocialLink.model';
+  import type { SocialLinkModel } from "@/models/SocialLink.model";
 
-	const { socialLink }: { socialLink: SocialLinkModel } = $props();
+  const { socialLink }: { socialLink: SocialLinkModel } = $props();
 </script>
 
 <a class="social-link overflow-hidden rounded-xl transition" href={socialLink.link} target="_blank">
-	<img alt={socialLink.name} src={socialLink.img} />
-	<span>{socialLink.name}</span>
+
+  {#await import(`/src/assets/logo/${socialLink.img}.svg`) then { default: src }}
+    <img {src} alt="{socialLink.name}" />
+  {/await}
+  <span>{socialLink.name}</span>
 </a>
 
 <style lang="scss">
-	.social-link {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		justify-content: flex-start;
-		text-align: start;
+  .social-link {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    text-align: start;
 
-		line-height: 1.2;
-		font-weight: 600;
+    line-height: 1.2;
+    font-weight: 600;
 
-		padding: 1rem;
-		gap: 0.75rem;
+    padding: 1rem;
+    gap: 0.75rem;
 
-		background-color: var(--primary-color-light);
-		color: var(--primary-color-dark);
-		&:hover,
-		&:focus {
-			background-color: var(--primary-color-lighter);
-		}
+    background-color: var(--primary-color-light);
+    color: var(--primary-color-dark);
 
-		& > img {
-			aspect-ratio: 1/1;
-			width: 1.5em;
-			height: 1.5em;
-			display: flex;
-			object-fit: contain;
-		}
-		& > span {
-			width: 100%;
-			flex-grow: 1;
-		}
-	}
+    &:hover,
+    &:focus {
+      background-color: var(--primary-color-lighter);
+    }
+
+    & > img {
+      aspect-ratio: 1/1;
+      width: 1.5em;
+      height: 1.5em;
+      display: flex;
+      object-fit: contain;
+    }
+
+    & > span {
+      width: 100%;
+      flex-grow: 1;
+    }
+  }
 </style>
