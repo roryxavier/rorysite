7a42d5c81bc65cf657780839175dea75165b3764
JimmyChane <63626658+JimmyChane@users.noreply.github.com>
2025-07-30 23:53:47 +0800

    update: social icon add: social background

--- src/models/Fursona.model.ts
+++ src/models/Fursona.model.ts
@@ -1,5 +1,8 @@
 import { IconModel } from './Icon.model';
 import type { SocialLinkModel } from './SocialLink.model';
+import { BackgroundModel } from './Background.model';
+
+export const USE_BACKGROUND = false;
 
 export type FursonaModel = Readonly<{
   labels: string[];
@@ -13,41 +16,59 @@ export const RORY_CHRIS_FURSONA: FursonaModel = {
       href: 'https://t.me/roryxavier',
       alt: 'Telegram',
       name: '@roryxavier',
-      icon: new IconModel('https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg'),
+      icon: new IconModel(() => import('~icon/telegram-color.svg')),
+      background: USE_BACKGROUND
+        ? new BackgroundModel(() => import('@/assets/page/home/telegram-social-background.jpg'))
+        : undefined,
     },
     {
       href: 'https://ref.rorywolf.net',
       alt: 'Reference Site',
       name: 'Reference Website',
-      icon: new IconModel('https://img.icons8.com/color/48/null/paint-palette-with-brush.png'),
+      icon: new IconModel(() => import('~icon/paint-palette-with-brush.png')),
+      background: USE_BACKGROUND
+        ? new BackgroundModel(() => import('@/assets/page/home/ref-social-background.png'))
+        : undefined,
     },
     {
       href: 'https://bsky.app/profile/roryxavier.bsky.social',
       alt: 'Bluesky',
       name: '@roryxavier',
-      icon: new IconModel('https://bsky.app/static/favicon-32x32.png'),
+      icon: new IconModel(() => import('~icon/bluesky-color-gradient.svg')),
     },
     {
       href: 'https://twitter.com/rory_xavi',
       alt: 'Twitter',
       name: '@rory_xavi',
-      icon: new IconModel(
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/150px-Logo_of_Twitter.svg.png',
-      ),
+      icon: new IconModel(() => import('~icon/twitter-color.svg')),
+      background: USE_BACKGROUND
+        ? new BackgroundModel(() => import('@/assets/page/home/twitter-social-background.jpg'))
+        : undefined,
     },
     {
       href: 'https://github.com/roryxavier',
       alt: 'Github',
       name: '@roryxavier',
-      icon: new IconModel(
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png',
-      ),
+      icon: new IconModel(() => import('~icon/github.svg'), {
+        invertColorOnTheme: 'dark',
+      }),
+      background: USE_BACKGROUND
+        ? new BackgroundModel(() => import('@/assets/page/home/github-social-background.png'))
+        : undefined,
     },
     {
       href: 'https://discordapp.com/users/525678985049538610',
       alt: 'Discord',
       name: '@roryxavier',
-      icon: new IconModel('https://www.svgrepo.com/show/353655/discord-icon.svg'),
+      icon: new IconModel(() => import('~icon/discord-color.svg')),
+    },
+    {
+      href: 'https://t.me/addstickers/RoryChris',
+      alt: 'Telegram Sticker Link',
+      name: 'Telegram Sticker',
+      icon: new IconModel(() => import('~icon/link-white.svg'), {
+        invertColorOnTheme: 'light',
+      }),
     },
   ],
 };
